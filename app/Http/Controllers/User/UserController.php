53c22b4af5eba6e10c022f6d86d1dffae21116ca
Lilian Burdianov <lilian.burdianov@gmail.com>
2018-12-19 22:05:14 +0400

    resending the verification email if requested

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\ApiController;
+use App\Mail\UserCreated;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends ApiController
 {
@@ -118,5 +120,14 @@ class UserController extends ApiController
         $user->save();
 
         return $this->showMessage('The account has been verified successfully');
+    }
+
+    public function resend(User $user)
+    {
+        if ($user->isVerified()) {
+            return $this->errorResponse('This user is already verified', Response::HTTP_CONFLICT);
+        }
+        Mail::to($user)->send(new UserCreated($user));
+        return $this->showMessage('The verification email has been resent');
     }
 }
